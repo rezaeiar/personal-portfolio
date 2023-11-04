317266db90629a96a05a722527258a191dff36cd
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-04 19:01:52 +0330

    fix: create Member Component

---
+++ src/components/Member/Member.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Member({ name, position, img }) {
+    return (
+        <div className='relative group'>
+            <img src={`./images/persons/${img}`} className='min-w-full' alt="person image" />
+            <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all absolute top-0 left-0 flex flex-col gap-y-5 justify-center items-center w-full h-full bg-gradient-to-r from-[#F8FAFF] from-0% via-[#EEF4FF] via-50% to-[#FFECEC] to-100%">
+                <h5 className='text-xl font-Inter-SemiBold'>
+                    {name}
+                </h5>
+                <span className='font-Inter-Regular text-[#454D55] text-xs'>
+                    {position}
+                </span>
+                <div className="flex gap-x-2.5">
+                    <i class="bi bi-facebook"></i>
+                    <i class="bi bi-linkedin"></i>
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-dribbble"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import Member from '../Member/Member'
+
 export default function Members() {
     const [data, setData] = useState([
         { id: 1, name: 'Anna Smith', position: 'Creative Director', img: 'Rectangle 13 (1).png' },
@@ -11,23 +13,7 @@ export default function Members() {
         <div className='py-6 lg:py-12 grid grid-cols-2 md:grid-cols-4'>
             {
                 data.map((member) => (
-                    <div className='relative group'>
-                        <img src={`./images/persons/${member.img}`} className='min-w-full' alt="" />
-                        <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all absolute top-0 left-0 flex flex-col gap-y-5 justify-center items-center w-full h-full bg-gradient-to-r from-[#F8FAFF] from-0% via-[#EEF4FF] via-50% to-[#FFECEC] to-100%">
-                            <h5 className='text-xl font-Inter-SemiBold'>
-                                {member.name}
-                            </h5>
-                            <span className='font-Inter-Regular text-[#454D55] text-xs'>
-                                {member.position}
-                            </span>
-                            <div className="flex gap-x-2.5">
-                                <i class="bi bi-facebook"></i>
-                                <i class="bi bi-linkedin"></i>
-                                <i class="bi bi-instagram"></i>
-                                <i class="bi bi-dribbble"></i>
-                            </div>
-                        </div>
-                    </div>
+                    <Member key={member.id} {...member} />
                 ))
             }
         </div>
