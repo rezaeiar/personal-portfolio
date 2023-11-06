6d65c090937b132da2e23f1d83fb81f97632c039
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-06 23:52:02 +0330

    fix: responsive PortfolioSection & PortfolioItem

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function PortfolioItem() {
     return (
-        <div className='flex gap-y-5 flex-col'>
+        <div className='flex gap-y-3 md:gap-y-4 lg:gap-y-5 flex-col'>
             <div>
                 <img src="./images/portfolio/Rectangle 101.png" className='w-full' alt="" />
             </div>
-            <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-y-3">
-                    <h1 className='text-[#344054] text-2xl font-Inter-Bold'>
+            <div className="flex justify-between items-center gap-x-2">
+                <div className="flex flex-col gap-y-2 lg:gap-y-3">
+                    <h1 className='text-[#344054] text-lg lg:text-xl xl:text-2xl font-Inter-Bold line-clamp-1'>
                         Specialty Restaurant Group
                     </h1>
-                    <span className='font-Inter-Regular text-[#667085]'>
+                    <span className='font-Inter-Regular text-xs lg:text-sm xl:text-base text-[#667085] line-clamp-1'>
                         Enriched Immigration Process
                     </span>
                 </div>
-                <div className='flex items-center gap-x-2 text-sm text-[#667085]'>
+                <div className='flex items-center gap-x-2 text-xs xl:text-sm text-[#667085] shrink-0'>
                     <span>
                         Mobile
                     </span>
--- src/components/PortfolioSection/PortfolioSection.jsx
+++ src/components/PortfolioSection/PortfolioSection.jsx
@@ -4,9 +4,9 @@ import PortfolioItem from '../PortfolioItem/PortfolioItem'
 
 export default function PortfolioSection() {
     return (
-        <div className='py-12'>
+        <div className='sm:py-6 md:py-12'>
             <div className="container">
-                <div className="grid grid-cols-2 gap-12">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-8 xl:gap-12">
                     <PortfolioItem />
                     <PortfolioItem />
                     <PortfolioItem />
