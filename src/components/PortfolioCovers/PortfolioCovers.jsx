942eb38805618e97984412123812b19ddcdae61d
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-06 22:32:28 +0330

    fix: responsive PortfolioCovers

--- src/components/PortfolioCovers/PortfolioCovers.jsx
+++ src/components/PortfolioCovers/PortfolioCovers.jsx
@@ -4,19 +4,19 @@ export default function PortfolioCovers() {
     return (
         <div className='pb-14'>
             <div className="container">
-                <div className="relative h-[364px] flex items-end mt-24">
-                    <div>
+                <div className="relative sm:h-[192px] md:h-[232px] lg:h-[310px] xl:h-[390px] 2xl:h-[460px] flex items-end sm:mt-20">
+                    <div className='hidden sm:flex'>
                         <div className="rounded-xl overflow-hidden absolute -right-12 top-0">
-                            <img src="./images/work/Rectangle 100.png" className='h-[340px]' alt="" />
+                            <img src="./images/work/Rectangle 100.png" className='h-[174px] md:h-[212px] lg:h-[276px] xl:h-[340px] 2xl:h-[410px]' alt="" />
                         </div>
-                        <div className="rounded-xl overflow-hidden absolute -left-2 -top-24">
-                            <img src="./images/work/Rectangle 99.png" className='h-[340px]' alt="" />
+                        <div className="rounded-xl overflow-hidden absolute -left-4 -top-12 md:-top-16 lg:-top-20">
+                            <img src="./images/work/Rectangle 99.png" className='h-[174px] md:h-[212px] lg:h-[276px] xl:h-[340px] 2xl:h-[410px]' alt="" />
                         </div>
                     </div>
-                    <div className="absolute -top-24 right-24 flex gap-x-7">
-                        <img src="./images/work/Frame.svg" className='h-fit ' alt="" />
-                        <img src="./images/work/Frame.svg" className='h-fit mt-12' alt="" />
-                        <img src="./images/work/Frame (2).svg" className='h-fit mt-4' alt="" />
+                    <div className="absolute -top-16 lg:-top-20 right-16 lg:right-24 hidden md:flex gap-x-7">
+                        <img src="./images/work/Frame.svg" className='h-fit w-6 lg:w-8' alt="" />
+                        <img src="./images/work/Frame.svg" className='h-fit mt-8 lg:mt-10 xl:mt-12 w-6 lg:w-8' alt="" />
+                        <img src="./images/work/Frame (2).svg" className='h-fit mt-2 xl:mt-4 w-10 lg:w-14' alt="" />
                     </div>
                     <img src="./images/work/Frame 78.svg" className='w-full' alt="" />
                 </div>
--- src/components/PortfolioHeader/PortfolioHeader.jsx
+++ src/components/PortfolioHeader/PortfolioHeader.jsx
@@ -4,17 +4,17 @@ import Button from '../Button/Button'
 
 export default function PortfolioHeader() {
     return (
-        <div className='py-20'>
+        <div className='py-12 md:py-20'>
             <div className="container">
                 <div className="flex flex-col items-center gap-y-2.5">
-                    <Button styles='block transition-colors px-3 lg:px-3.5 py-2.5 bg-[#F4F8FF] text-[#34113F] rounded-md font-Inter-Medium text-xs lg:text-sm'>
+                    <Button styles='block transition-colors px-3 lg:px-3.5 py-2 lg:py-2.5 bg-[#F4F8FF] text-[#34113F] rounded-md font-Inter-Medium text-xs lg:text-sm'>
                         Our Work
                     </Button>
-                    <h4 className='text-[#34113F] font-Inter-Bold text-5xl'>
+                    <h4 className='text-[#34113F] font-Inter-Bold text-3xl md:text-5xl'>
                         Portfolio
                     </h4>
-                    <span className='text-base font-Inter-Regular text-[#475467] text-center'>
-                        12 — SaaS companies we’ve helped in the past 24 months. Our holistic process <br />
+                    <span className='text-xs sm:text-sm md:text-base font-Inter-Regular text-[#475467] text-center'>
+                        12 — SaaS companies we’ve helped in the past 24 months. Our holistic process <br className='hidden sm:block' />
                         takes care of discovery, branding, design, and development.
                     </span>
                 </div>
