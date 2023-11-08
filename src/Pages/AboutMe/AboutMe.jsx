c50a4f01e4aebd6e3fb640a767bad5f551160f53
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-08 20:15:48 +0330

    fix: responsive AboutMe

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -4,6 +4,7 @@ import AboutMeHeader from '../../components/AboutMeHeader/AboutMeHeader'
 import DetailsSection from '../../components/DetailsSection/DetailsSection'
 import StatisticsSection from '../../components/StatisticsSection/StatisticsSection'
 import Members from '../../components/Members/Members'
+import AboutMeCover from '../../components/AboutMeCover/AboutMeCover'
 
 import Button from '../../components/Button/Button'
 
@@ -12,22 +13,22 @@ export default function AboutMe() {
         <>
             <AboutMeHeader />
             <DetailsSection>
-                <div className="flex flex-col lg:flex-row justify-between items-center gap-x-20">
-                    <div className="relative shrink-0">
-                        <img src="./images/home/Rectangle 12.png" className='h-[455px] w-[493px] object-cover' alt="" />
+                <div className="flex flex-col lg:flex-row justify-between items-center gap-x-20 gap-y-4 lg:gap-y-0">
+                    <div className="relative shrink-0 w-full lg:w-auto">
+                        <img src="./images/home/Rectangle 12.png" className='h-[390px] sm:h-[500px] md:h-[600px] lg:h-[455px] w-full lg:w-[455px] xl:w-[493px] object-cover' alt="" />
                     </div>
-                    <div className="flex flex-col gap-y-5 lg:gap-y-8">
-                        <div className="flex flex-col gap-y-4">
-                            <h2 className='font-Inter-Bold text-[#34113F] text-4xl'>
-                                Infuse Your Brand with <br className='hidden sm:block' />
+                    <div className="flex flex-col gap-y-4 sm:gap-y-8">
+                        <div className="flex flex-col gap-y-3 sm:gap-y-4">
+                            <h2 className='font-Inter-Bold text-[#34113F] text-2xl sm:text-4xl'>
+                                Infuse Your Brand with <br className='hidden lg:block' />
                                 Excellence
                             </h2>
-                            <span className='text-[#454D55] font-Inter-Regular text-sm md:text-base'>
+                            <span className='text-[#454D55] font-Inter-Regular text-xs sm:text-sm md:text-base'>
                                 Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. Wiusmod tempor incididunt.Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             </span>
                         </div>
-                        <div className="flex flex-col gap-y-7">
-                            <ul className='flex flex-col sm:flex-row justify-between lg:flex-col gap-y-4 font-Inter-Medium text-base md:text-lg'>
+                        <div className="flex flex-col gap-y-4 sm:gap-y-7">
+                            <ul className='flex flex-col  justify-between lg:flex-col gap-y-2 sm:gap-y-4 font-Inter-Medium text-sm sm:text-base md:text-lg'>
                                 <li className='flex items-center gap-x-2'>
                                     <i class="bi bi-clock"></i>
                                     Adipiscing eli sed eiusmod
@@ -57,7 +58,7 @@ export default function AboutMe() {
                 <Members />
             </div>
             <DetailsSection>
-                <div className="flex flex-col lg:flex-row justify-between items-center gap-x-32">
+                <div className="flex flex-col lg:flex-row justify-between items-center gap-x-32 py-6 lg:py-8">
                     <div className="relative mb-5 sm:mb-8 lg:mb-14 lg:mr-48 shrink-0">
                         <img src="./images/home/Rectangle 12.png" className='hidden lg:block' alt="" />
                         <img src="./images/home/figure-150x150 1.svg" className='hidden lg:block absolute top-14 -right-48' alt="" />
@@ -75,17 +76,17 @@ export default function AboutMe() {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-y-5 lg:gap-y-8">
-                        <span className='text-[#454D55] font-Inter-Regular text-sm md:text-base'>
+                    <div className="flex flex-col gap-y-4 lg:gap-y-8">
+                        <span className='text-[#454D55] font-Inter-Regular text-xs lg:text-base'>
                             HOW WE WORK
                         </span>
-                        <h5 className='font-Inter-Bold text-[#34113F] text-4xl'>
+                        <h5 className='font-Inter-Bold text-[#34113F] text-2xl sm:text-3xl md:text-4xl'>
                             Our Design Process in Four Steps
                         </h5>
                         <span className='text-[#454D55] font-Inter-Regular text-sm md:text-base'>
                             Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
                         </span>
-                        <ul className='flex flex-col sm:flex-row justify-between lg:flex-col gap-y-4 font-Inter-SemiBold text-[#A6A6AA] text-base md:text-xl'>
+                        <ul className='flex flex-col justify-between lg:flex-col gap-y-4 font-Inter-SemiBold text-[#A6A6AA] text-sm sm:text-base md:text-xl'>
                             <li className='text-black'>
                                 01. Client Brief
                             </li>
@@ -102,6 +103,7 @@ export default function AboutMe() {
                     </div>
                 </div>
             </DetailsSection>
+            <AboutMeCover />
         </>
     )
 }
---
+++ src/components/AboutMeCover/AboutMeCover.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutMeCover() {
+    return (
+        <div className=''>
+            <img src="./images/about-me/Rectangle 113.png" className='max-h-[670px] object-cover' alt="" />
+        </div>
+    )
+}
