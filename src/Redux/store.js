ce16baffa8d49babdd59cf975f328c74670d6d85
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-10 14:22:50 +0330

    feat: change Testimonials to Dynamic

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,13 +3,15 @@ import menusReducer from '../Redux/store/menus.js'
 import projectsReducer from '../Redux/store/projects.js'
 import statisticsReducer from '../Redux/store/Statistics.js'
 import membersReducer from '../Redux/store/members.js'
+import testimonialsReducer from '../Redux/store/testimonials.js'
 
 const store = configureStore({
     reducer: {
         menus: menusReducer,
         projects: projectsReducer,
         statistics: statisticsReducer,
-        members: membersReducer
+        members: membersReducer,
+        testimonials: testimonialsReducer
     }
 })
 
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper/modules';
@@ -6,7 +6,18 @@ import { Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { getTestimonialsFromServer } from '../../Redux/store/testimonials';
+
 export default function Testimonials() {
+
+    const testimonials = useSelector(state => state.testimonials)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getTestimonialsFromServer())
+    }, [])
+
     return (
         <div className='py-12 lg:py-20'>
             <div className="container">
@@ -32,77 +43,48 @@ export default function Testimonials() {
                                 disableOnInteraction: false,
                             }}
                         >
-                            <SwiperSlide>
-                                <div className="flex mt-8 lg:mt-10 bg-[#F6F1F7] rounded-2xl grow p-6 lg:p-10 flex-col gap-y-3 sm:gap-y-6 lg:gap-y-8 relative">
-                                    <img src="./images/about-me/“.svg" className='absolute h-8 sm:h-auto left-8 sm:left-12 -top-4 sm:-top-8 lg:-top-10' alt="" />
-                                    <div className="flex justify-between items-center gap-x-2 sm:gap-x-8 lg:gap-x-16">
-                                        <p className='font-Inter-Regular text-xs sm:text-xl lg:text-2xl text-[#34113F]'>
-                                            Everyone on the team is expected to meet the
-                                            same high standards and produce work of the
-                                            highest calibre, Everyone on the team is expected to meet the
-                                            same high standards
-                                        </p>
-                                        <div className="shrink-0">
-                                            <img src="./images/about-me/Rectangle 108.png" className='h-32 sm:h-auto' alt="" />
-                                        </div>
-                                    </div>
-                                    <span className='h-px w-full bg-[#E8DBEC]'></span>
-                                    <div className="flex justify-between items-center">
-                                        <div className="flex gap-x-3">
-                                            <div className="rounded-full overflow-hidden">
-                                                <img src="./images/about-me/Rectangle 5.png" className='' alt="" />
+                            {
+                                testimonials.map(testimonial => (
+                                    <SwiperSlide key={testimonial.id}>
+                                        <div className="flex mt-8 lg:mt-10 bg-[#F6F1F7] rounded-2xl grow p-6 lg:p-10 flex-col gap-y-3 sm:gap-y-6 lg:gap-y-8 relative">
+                                            <img src="./images/about-me/“.svg" className='absolute h-8 sm:h-auto left-8 sm:left-12 -top-4 sm:-top-8 lg:-top-10' alt="" />
+                                            <div className="flex justify-between items-center gap-x-2 sm:gap-x-8 lg:gap-x-16">
+                                                <p className='font-Inter-Regular text-xs sm:text-xl lg:text-2xl text-[#34113F]'>
+                                                    {
+                                                        testimonial.description
+                                                    }
+                                                </p>
+                                                <div className="shrink-0">
+                                                    <img src={`./images/about-me/${testimonial.HeaderPhoto}`} className='h-32 sm:h-auto' alt="" />
+                                                </div>
                                             </div>
-                                            <div className="flex flex-col gap-y-1 justify-center">
-                                                <span className='font-Inter-Regular text-xs sm:text-sm'>
-                                                    Michael B. Jordan
-                                                </span>
-                                                <h6 className='font-Inter-Bold text-sm sm:text-base'>
-                                                    VP of Marketing, disney
-                                                </h6>
-                                            </div>
-                                        </div>
-                                        <div className='w-16 sm:w-auto'>
-                                            <img src="./images/about-me/Layer_1.svg" alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="flex mt-8 lg:mt-10 bg-[#F6F1F7] rounded-2xl grow p-6 lg:p-10 flex-col gap-y-3 sm:gap-y-6 lg:gap-y-8 relative">
-                                    <img src="./images/about-me/“.svg" className='absolute h-8 sm:h-auto left-8 sm:left-12 -top-4 sm:-top-8 lg:-top-10' alt="" />
-                                    <div className="flex justify-between items-center gap-x-2 sm:gap-x-8 lg:gap-x-16">
-                                        <p className='font-Inter-Regular text-xs sm:text-xl lg:text-2xl text-[#34113F]'>
-                                            Everyone on the team is expected to meet the
-                                            same high standards and produce work of the
-                                            highest calibre, Everyone on the team is expected to meet the
-                                            same high standards
-                                        </p>
-                                        <div className="shrink-0">
-                                            <img src="./images/about-me/Rectangle 108.png" className='h-32 sm:h-auto' alt="" />
-                                        </div>
-                                    </div>
-                                    <span className='h-px w-full bg-[#E8DBEC]'></span>
-                                    <div className="flex justify-between items-center">
-                                        <div className="flex gap-x-3">
-                                            <div className="rounded-full overflow-hidden">
-                                                <img src="./images/about-me/Rectangle 5.png" className='' alt="" />
-                                            </div>
-                                            <div className="flex flex-col gap-y-1 justify-center">
-                                                <span className='font-Inter-Regular text-xs sm:text-sm'>
-                                                    Michael B. Jordan
-                                                </span>
-                                                <h6 className='font-Inter-Bold text-sm sm:text-base'>
-                                                    VP of Marketing, disney
-                                                </h6>
+                                            <span className='h-px w-full bg-[#E8DBEC]'></span>
+                                            <div className="flex justify-between items-center">
+                                                <div className="flex gap-x-3">
+                                                    <div className="rounded-full overflow-hidden">
+                                                        <img src={`./images/about-me/${testimonial.profilePhoto}`} className='' alt="" />
+                                                    </div>
+                                                    <div className="flex flex-col gap-y-1 justify-center">
+                                                        <span className='font-Inter-Regular text-xs sm:text-sm'>
+                                                            {
+                                                            testimonial.name
+                                                            }
+                                                        </span>
+                                                        <h6 className='font-Inter-Bold text-sm sm:text-base'>
+                                                            {
+                                                                testimonial.testimonial
+                                                            }
+                                                        </h6>
+                                                    </div>
+                                                </div>
+                                                <div className='w-16 sm:w-auto'>
+                                                    <img src={`./images/about-me/${testimonial.company}`} className='w-16' alt="" />
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className='w-16 sm:w-auto'>
-                                            <img src="./images/about-me/Layer_1.svg" alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-
+                                    </SwiperSlide>
+                                ))
+                            }
                         </Swiper>
                         <div className="hidden lg:flex testimonials-next flex-items-center justify-center bg-[#F4F8FF] rounded-3xl cursor-pointer">
                             <i className="bi bi-arrow-right p-3"></i>
