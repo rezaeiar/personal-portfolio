59239c25af1a901e6311409d4afc867d09b863c7
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-11 14:02:01 +0330

    feat: create statistics

--- src/Redux/store/testimonials.js
+++ src/Redux/store/testimonials.js
@@ -20,26 +20,3 @@ const slice = createSlice({
 })
 
 export default slice.reducer
-
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-// import apiRequests from '../../services/axios/configs/configs'
-
-// export const getStatisticsFromServer = createAsyncThunk(
-//     'statistics/getStatisticsFromServer',
-//     async () => {
-//         return apiRequests('/statistics').then(res => res.data)
-//     }
-// )
-// const slice = createSlice({
-//     name: 'statistics',
-//     initialState: [],
-//     reducers: {},
-
-//     extraReducers: (builder) => {
-//         builder.addCase(getStatisticsFromServer.fulfilled, (state, action) => {
-//             return action.payload
-//         })
-//     }
-// })
-
-// export default slice.reducer
