30f4014271e29d506fd33aebd08de350735ac0d2
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-11 14:27:22 +0330

    feat: create ContactUsHeader

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import useScroll from '../../hooks/useScroll'
 
+import ContactUsHeader from '../../components/ContactUsHeader/ContactUsHeader'
+
 export default function ContactUs() {
 
     useScroll()
     return (
         <>
-            
+            <ContactUsHeader />
         </>
     )
 }
---
+++ src/components/ContactUsHeader/ContactUsHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ContactUsHeader() {
+    return (
+        <div className='pt-12 pb-4 md:py-20'>
+            <div className="container">
+                <div className="flex flex-col items-center gap-y-2.5">
+                    <h4 className='text-[#34113F] font-Inter-Bold text-3xl md:text-5xl'>
+                        Contact Us
+                    </h4>
+                    <i className="bi bi-arrow-down text-xl md:text-2xl"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
