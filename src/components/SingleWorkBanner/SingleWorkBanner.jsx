6c61c39c23d592aa2fd90f48a71819f9f94ec820
[rezaeiar] <rezaeiar0@gmail.com>
2023-11-16 12:14:23 +0330

    feat: create SingleWorkBanner

---
+++ src/components/SingleWorkBanner/SingleWorkBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SingleWorkBanner() {
+    return (
+        <div className='w-full'>
+            <img src="/images/single-work/style-tribe-banner 1.png" className='w-full' alt="" />
+        </div>
+    )
+}
